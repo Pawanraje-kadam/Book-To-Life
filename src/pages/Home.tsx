223e44399afbd01b18fecf51e6b17c92fe8a9055
Pawanraje Kadam <kep717680@gmail.com>
2026-06-10 16:07:18 +0530

    Integrate Navbar and Footer into Home component
    Added Navbar and Footer components to Home page layout.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import { Hero } from "@/components/home/Hero";
 import { Features } from "@/components/home/Features";
+import { Navbar } from "@/components/layout/Navbar";
+import { Footer } from "@/components/layout/Footer";
 
 export function Home() {
   return (
-    <>
+    <div className="relative">
+      {/* Navbar overlaid on hero */}
+      <div className="absolute top-0 left-0 w-full z-20">
+        <Navbar />
+      </div>
       <Hero />
       <Features />
-    </>
+      <Footer />
+    </div>
   );
 }
