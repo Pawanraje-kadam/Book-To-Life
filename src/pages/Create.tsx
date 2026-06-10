1be75f817a765a7e666ca064bbbb5b936535323e
Pawanraje Kadam <kep717680@gmail.com>
2026-06-10 16:08:50 +0530

    Refactor Create component layout with Navbar and Footer

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,9 +1,15 @@
 import { PredictionForm } from "@/components/features/PredictionForm";
+import { Navbar } from "@/components/layout/Navbar";
+import { Footer } from "@/components/layout/Footer";
 
 export function Create() {
   return (
-    <div className="pt-24 pb-12 bg-background min-h-screen">
-      <PredictionForm />
+    <div className="min-h-screen flex flex-col" style={{ background: 'hsl(201, 100%, 8%)' }}>
+      <Navbar />
+      <main className="flex-grow pt-8 pb-20">
+        <PredictionForm />
+      </main>
+      <Footer />
     </div>
   );
 }
