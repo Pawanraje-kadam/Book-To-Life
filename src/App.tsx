1823dc7287a12de22a2972d562774304c343fde8
Pawanraje Kadam <kep717680@gmail.com>
2026-06-10 16:06:12 +0530

    Change ThemeProvider defaultTheme to 'dark'

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
 import { Home } from "@/pages/Home";
 import { Create } from "@/pages/Create";
 
 export default function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="book-to-life-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="book-to-life-theme">
       <BrowserRouter>
         <div className="relative min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/create" element={<Create />} />
-            </Routes>
-          </main>
-          <Footer />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </ThemeProvider>
